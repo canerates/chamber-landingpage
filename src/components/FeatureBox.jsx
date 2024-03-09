80846decff62f4a3e025752e291df4f9e5b3625b
Caner Ates <cnrates@gmail.com>
2024-03-09 19:26:33 +0300

    Features update

--- src/components/FeatureBox.jsx
+++ src/components/FeatureBox.jsx
@@ -1,10 +1,8 @@
-// import { Button, Col } from 'reactstrap';
 import { Col } from 'reactstrap';
 
 const FeatureBox = (props) => {
     return(
         <Col lg='4' md='6' className='feature-box-container' key={`features-${props.index}`}>
-            {/* <div onClick={() => props.handler(props.index)} className='feature-box'> */}
             <div className='feature-box'>
                 <div className='feature-box-img-block'>
                     <img alt='' className='feature-box-img' src={props.icon} />
@@ -12,7 +10,6 @@ const FeatureBox = (props) => {
                 <div className='feature-box-description'>
                     <h4 className='feature-box-text'>{props.title}</h4>
                     <p>{props.details}</p>
-                    {/* <Button size='sm' className='feature-box-button'>Check Details</Button> */}
                 </div>
             </div>
         </Col>
--- src/pages/Features.jsx
+++ src/pages/Features.jsx
@@ -1,6 +1,4 @@
-import { useState } from 'react';
-import { Container, Row, Modal } from 'reactstrap';
-import ReactPlayer from 'react-player';
+import { Container, Row } from 'reactstrap';
 import SectionTitle from '../common/SectionTitle';
 import FeatureBox from "../components/FeatureBox";
 import featureIcon1 from '../assets/images/features/feature_icon1.png';
@@ -9,12 +7,6 @@ import featureIcon3 from '../assets/images/features/feature_icon3.png';
 import featureIcon4 from '../assets/images/features/feature_icon4.png';
 import featureIcon5 from '../assets/images/features/feature_icon5.png';
 import featureIcon6 from '../assets/images/features/feature_icon6.png';
-import featureVideo1 from '../assets/videos/placeholder.MP4';
-import featureVideo2 from '../assets/videos/layout.mov';
-import featureVideo3 from '../assets/videos/pbr.mov';
-import featureVideo4 from '../assets/videos/placeholder.MP4';
-import featureVideo5 from '../assets/videos/3dmodels.mov';
-import featureVideo6 from '../assets/videos/lighting.MP4';
 
 
 const pageData = {
@@ -24,54 +16,37 @@ const pageData = {
         {
             icon: featureIcon1,
             title: 'Measure',
-            video: featureVideo1,
             details: 'Measure the floor, walls and any real-world length by creating AR shapes to show length information in centimeters.'
         },
         {
             icon: featureIcon2,
             title: 'AR Layout',
-            video: featureVideo2,
             details: 'Generate 3D virtual room layout model in real-time by defining the corner positions of the real-world room.'
         },
         {
             icon: featureIcon3,
             title: 'PBR Texture',
-            video: featureVideo3,
             details: 'Use PBR textures to apply to generated virtual floor and walls plane to give realistic looking for the 3D room layout.'
         },
         {
             icon: featureIcon4,
             title: 'Scan Texture',
-            video: featureVideo4,
             details: 'Scan and modify the real-world textures to generate custom textures for the virtual floor and walls planes in AR room.'
         },
         {
             icon: featureIcon5,
             title: '3D Models',
-            video: featureVideo5,
             details: 'Place and manipulate the pre-defined 3d USDZ furniture models to augmented room layout to design in real-time.'
         },
         {
             icon: featureIcon6,
             title: 'Lighting',
-            video: featureVideo6,
             details: 'Use lighting options such as directional, point, spot lighting types to give realistic looking to the 3D AR room layout.'
         }
     ]
 };
 
 const Features = () => {
-    const [isOpen, setOpen] = useState(false);
-    const [selectedIndex, setSelectedIndex] = useState(0);
-
-    const openModal = (index) => {
-        setOpen(true);
-        setSelectedIndex(index);
-    }
-
-    const closeModal = () => {
-        setOpen(false);
-    }
 
     return(
         <section id='features'>
@@ -81,22 +56,10 @@ const Features = () => {
                 </Row>
                 <Row>
                     {pageData.features.map( (item, index) => 
-                        <FeatureBox handler={openModal} index={index} icon={item.icon} title={item.title} details={item.details}/>
+                        <FeatureBox index={index} icon={item.icon} title={item.title} details={item.details}/>
                     )}
                 </Row>
-                {/* <ModalVideo channel='custom' url={pageData.features[selectedIndex].video} ratio={modalRatio} autoplay={1} isOpen={isOpen} onClose={() => closeModal()} /> */}
-                <Modal isOpen={isOpen} toggle={closeModal} backdrop='static' keyboard={false}>
-                    <button onClick={closeModal} className='modal-video-close-btn'></button>
-                    <ReactPlayer
-                        url={pageData.features[selectedIndex].video}
-                        width='100%'
-                        height='calc(100vh - 100px)'
-                        controls={true}
-                        playing={isOpen}
-                        volume={1}
-                        muted={true}
-                    />
-                </Modal>
+                
             </Container>
         </section>
         
